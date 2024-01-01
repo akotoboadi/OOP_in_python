c57687ff14e1f7eb3710e2568542bf26e92532d2
akotoboadi <akotoboadi18@gmail.com>
2024-01-01 15:27:23 +0000

    imported the datetime module and tried it

---
+++ date_time.py
@@ -0,0 +1,11 @@
+# date and time in python 
+import datetime
+x= datetime.datetime.now()
+print(x)
+print(x.year)
+print(x.day)
+print(x.hour)
+print(x.minute)
+y =  datetime.datetime(2022,5,17)
+print(y)
+#in python the datetime module has to be imported before we can use it 
